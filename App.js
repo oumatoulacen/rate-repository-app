c1390f4dca60e1a53799dd80bb846803f3b60e1e
oumatoulacen <username@users.noreply.github.com>
2025-08-30 20:00:40 +0100

    Exercise 10.2

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<ImageBackground
+			source={require('./assets/background.jpg')}
+			style={styles.image}
+		>
+			<View style={styles.container}>
+				<Text>Welcome To the Show</Text>
+				<StatusBar style="auto" />
+			</View>
+		</ImageBackground>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#d57373ff',
+		color: 'red',
+		alignItems: 'center',
+		justifyContent: 'center',
+	},
+	image: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
 });
